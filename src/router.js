1de1e52376059ea6f8c415b794422c9fc764d263
dianjie <994833478@qq.com>
2016-10-09 21:21:58 +0800

    Revert "更新"
    This reverts commit 0722a11c67475a0a48fcae712f2f7a48f93cdba2.

--- src/router.js
+++ src/router.js
@@ -4,8 +4,6 @@ import {getSessionData, setSessionData, removeSessionData} from './system/system
 import LayOut from './page/public/layOut'
 import Mine from './page/mine/mine'
 import Index from './page/index/index'
-import Finance from './page/finance/finance'
-import Report from './page/report/report'
 class RouterContent extends Component {
     render() {
         const self = this;
@@ -15,8 +13,6 @@ class RouterContent extends Component {
                     <IndexRedirect to="index" />
                     <Route path="mine" component={Mine}/>
                     <Route path="index" component={Index}/>
-                    <Route path="finance" component={Finance}/>
-                    <Route path="report" component={Report}/>
                 </Route>
             </Router>
         )
